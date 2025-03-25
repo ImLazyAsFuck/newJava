3669a813bad8d7d0383ebb1414a697cf5b20f620
CanhVeTaiGia <lvy16012005@gmail.com>
2025-03-25 10:30:31 +0700

    add

--- ss18/B7.java
+++ ss18/B7.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class B7{
     public static void main(String[] args) {
@@ -27,5 +25,25 @@ public class B7{
             }
         }
         counts.forEach((k, v) -> System.out.println(k + " : " + v));
+
+        List<int[]> sortedList = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            sortedList.add(new int[]{entry.getKey(), entry.getValue()});
+        }
+
+        for (int i = 0; i < sortedList.size() - 1; i++) {
+            for (int j = 0; j < sortedList.size() - i - 1; j++) {
+                if (sortedList.get(j)[1] > sortedList.get(j + 1)[1]) {
+                    int[] temp = sortedList.get(j);
+                    sortedList.set(j, sortedList.get(j + 1));
+                    sortedList.set(j + 1, temp);
+                }
+            }
+        }
+
+        System.out.println("Các phần tử theo số lần xuất hiện tăng dần:");
+        for (int[] entry : sortedList) {
+            System.out.println(entry[0] + " : " + entry[1]);
+        }
     }
 }
